0d069a28973b062ca67653c1c6ef379e67733d62
ruthless <447758919@qq.com>
2022-06-22 16:47:14 +0800

    王俊博第一次提交

---
+++ src/main/java/com/example/shuqishixun/ShuqishixunApplication.java
@@ -0,0 +1,13 @@
+package com.example.shuqishixun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuqishixunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuqishixunApplication.class, args);
+	}
+
+}
