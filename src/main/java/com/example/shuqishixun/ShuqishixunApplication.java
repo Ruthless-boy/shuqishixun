ee3715b6bc52d35a5a2653a437e8b0035c7bc75f
ruthless <447758919@qq.com>
2022-06-22 18:18:51 +0800

    shahg

--- src/main/java/com/example/shuqishixun/ShuqishixunApplication.java
+++ src/main/java/com/example/shuqishixun/ShuqishixunApplication.java
@@ -1,8 +1,12 @@
 package com.example.shuqishixun;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@MapperScan("com.example.shuqishixun.dao")
 @SpringBootApplication
 public class ShuqishixunApplication {
 
